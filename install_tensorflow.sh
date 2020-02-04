53a7f5a5e55481e5cd26081d03f38d0f051d84a2
Ben Zinberg <bzinberg@alum.mit.edu>
2020-02-04 18:07:08 -0500

    oh right I need to undelete `install_tensorflow.sh` too

---
+++ install_tensorflow.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+apt-get update -qq && apt-get install -qq \
+    build-essential \
+    python3 \
+    python-virtualenv
+virtualenv -p python3 tf_env
+. tf_env/bin/activate
+pip install --upgrade pip
+pip install --upgrade tensorflow
+deactivate
